6962d7646a8b189697d2f26a07a44487ce3c16be
Maria Pacana <maria.pacana@gmail.com>
2014-12-21 13:25:31 -0800

    Tracks and displays current calculation.
    * When a button is clicked, add its value (number or operator)
      to "tokens" array.
    * When a button is clicked, update display and show updated
      string of tokens.
    * If "clear" button is clicked, reset token array.

--- elements/calculator/calculator.js
+++ elements/calculator/calculator.js
@@ -1,11 +1,34 @@
 Polymer({
-  // ready: function () {
-  //   console.log('this', this);
-  //   console.log('polymer element', this.$);
-  // },
-  // buttonClick: function (event, detail, sender) { 
-  //   console.log("event", event);
-  //   console.log("detail", detail);
-  //   console.log("sender", sender);
-  // }
+  ready: function () {
+    this.tokens = [];
+  },
+  operations: {
+    'multiply': 'x',
+    'add': '+',
+    'subtract': '-',
+    'divide': '/'
+  },
+  calculationExpression: function () {
+    var self = this;
+    return this.tokens.map(function(elem){
+      return (self.operations[elem]) ? self.operations[elem] : elem; 
+    }).join("");
+  },
+  calculate: function () {
+    console.log("calculating things");
+  },
+  addToCalculation: function (event, detail, sender) { 
+    this.tokens.push(sender.value);
+    this.displayCurrentCalculation();
+  },
+  clearCalculation: function() {
+    this.tokens = [];
+    this.displayCurrentCalculation();
+  },
+  displayCurrentCalculation: function() {
+    this.$.calculation.value = this.calculationExpression();
+  },
+  computed: {
+    currentCalculationString: 'bar'
+  }
 });
