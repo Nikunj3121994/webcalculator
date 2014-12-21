7df091125c88904414c634fdff9f539395549b93
Maria Pacana <maria.pacana@gmail.com>
2014-12-21 12:36:44 -0800

    Set up calculator keys.

---
+++ elements/calculator/calculator.js
@@ -0,0 +1,11 @@
+Polymer({
+  // ready: function () {
+  //   console.log('this', this);
+  //   console.log('polymer element', this.$);
+  // },
+  // buttonClick: function (event, detail, sender) { 
+  //   console.log("event", event);
+  //   console.log("detail", detail);
+  //   console.log("sender", sender);
+  // }
+});
