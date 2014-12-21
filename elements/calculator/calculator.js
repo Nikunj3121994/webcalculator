74b72d209878e51686edd810c4d8440141077cf6
Maria Pacana <maria.pacana@gmail.com>
2014-12-21 14:31:33 -0800

    Displays value of current calculation.
    * Pressing "=" evaluates the calculation and shows the result.
    * Tokens are now tracked in a string instead of an array.

--- elements/calculator/calculator.js
+++ elements/calculator/calculator.js
@@ -1,34 +1,35 @@
 Polymer({
   ready: function () {
-    this.tokens = [];
+    this.currentCalculation = "";
   },
-  operations: {
-    'multiply': 'x',
+  basicOperations: {
+    'multiply': '*',
     'add': '+',
     'subtract': '-',
-    'divide': '/'
+    'divide': '/',
   },
-  calculationExpression: function () {
-    var self = this;
-    return this.tokens.map(function(elem){
-      return (self.operations[elem]) ? self.operations[elem] : elem; 
-    }).join("");
+  currentCalculationString: function() {
+    return this.currentCalculation.replace("Math.sqrt(", "\u221A(")
   },
   calculate: function () {
-    console.log("calculating things");
+    this.$.calculation.value = eval(this.currentCalculation);
+    this.currentCalculation = this.$.calculation.value.toString();
   },
   addToCalculation: function (event, detail, sender) { 
-    this.tokens.push(sender.value);
+    if (sender.value == "sqrt") {
+      this.currentCalculation += "Math.sqrt("
+    } else if (this.basicOperations[sender.value]) {
+      this.currentCalculation += this.basicOperations[sender.value];
+    } else {
+      this.currentCalculation += sender.value;
+    }
     this.displayCurrentCalculation();
   },
   clearCalculation: function() {
-    this.tokens = [];
+    this.currentCalculation = "";
     this.displayCurrentCalculation();
   },
   displayCurrentCalculation: function() {
-    this.$.calculation.value = this.calculationExpression();
+    this.$.calculation.value = this.currentCalculationString();
   },
-  computed: {
-    currentCalculationString: 'bar'
-  }
 });
